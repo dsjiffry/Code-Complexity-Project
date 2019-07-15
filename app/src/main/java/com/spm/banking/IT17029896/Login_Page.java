4a9b60c7b280698ee3a27f219912616aa1270fa1
Shehan Jiffry <dsjiffry@gmail.com>
2019-07-15 09:24:39 +0530

    Initial
    Signed-off-by: Shehan Jiffry <dsjiffry@gmail.com>

---
+++ app/src/main/java/com/spm/banking/IT17029896/Login_Page.java
@@ -0,0 +1,15 @@
+package com.spm.banking.IT17029896;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spm.banking.R;
+
+public class Login_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__page);
+    }
+}
